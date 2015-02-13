04ad03751ec6c7fb18ec488b091783760cfb6164
sheez <blaat2k@mail.com>
2015-02-13 17:06:59 +0100

    add header, add quick animation, fix section headers

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+// (function() {
+
+// 	$(window).scroll(function(event) {
+// 		var y = $(this).scrollTop();
+
+// 		if(y === 0) {
+// 			$("nav").slideUp(700, function(){
+// 				return this;
+// 			});
+// 		}
+
+// 		if (y >= 1) {
+// 			$("nav").slideDown(700, function(){
+// 				return this;
+// 			});
+// 		}
+// 	});
+// })();
